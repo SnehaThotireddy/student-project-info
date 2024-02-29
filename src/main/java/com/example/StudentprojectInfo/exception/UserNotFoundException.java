9c2f5080731b9ec64d5e8a9c10bf0e09bf6a3220
Sneha Thotireddy <snehathotireddy@gmai.com>
2024-02-29 22:06:45 +0530

    first commit

---
+++ src/main/java/com/example/StudentprojectInfo/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.StudentprojectInfo.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
